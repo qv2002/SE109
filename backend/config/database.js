74abeb8998568c7350c946450f71c9b7fda20cb4
Kimline <93004199+Kimlinegit@users.noreply.github.com>
2023-05-31 03:55:04 +0700

    update UI, profile Img, NavBar

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,29 @@
-const mongooes = require("mongoose");
+// const mongooes = require("mongoose");
 
 
-const connectDatabase = () => {
-    mongooes.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then((data) => {
-        console.log(`Mongodb connected with server: ${data.connect.host}`);
-    })
-}
+// const connectDatabase = () => {
+//     mongooes.connect(process.env.DB_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }).then((data) => {
+//         console.log(`Mongodb connected with server: ${data.connect.host}`);
+//     })
+// }
 
-module.exports = connectDatabase
+// module.exports = connectDatabase
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI)
+        console.log("connected to db")
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+module.exports = connectDatabase;
--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,44 +1,98 @@
-import React from "react";
-import { ReactNavbar } from "overlay-navbar"
+// import React from "react";
+// import { ReactNavbar } from "overlay-navbar"
+// import logo from "../../../image/logo.png";
+
+// const options = {
+
+//     logoWidth: "20vmax",
+//     logo,
+//     burgerColor: "rgba(0, 0, 0, 0.616)",
+//     burgerColorHover: " black",
+//     navColor1: "white",
+//     logoHoverSize: "10px",
+//     logoHoverColor: " rgba(0, 0, 0, 0.616)",
+//     link1Text: "Trang chủ",
+//     link2Text: "Sản phẩm",
+//     link3Text: "Liên hệ",
+//     link4Text: "Chi tiết",
+//     link1Url: "/",
+//     link2Url: "/products",
+//     link3Url: "/contact",
+//     link4Url: "/about",
+//     link1Size: "1.3vmax",
+//     link1Color: "rgba(0, 0, 0, 0.616)",
+//     nav1justifyContent: "flex-end",
+//     nav2justifyContent: "flex-end",
+//     nav3justifyContent: "flex-start",
+//     nav4justifyContent: "flex-start",
+//     link1ColorHover: "black",
+//     link1Margin: "1vmax",
+//     profileIconUrl: "/login",
+//     profileIconColor: "rgba(0, 0, 0, 0.616)",
+//     searchIconColor: "rgba(0, 0, 0, 0.616)",
+//     cartIconColor: "rgba(0, 0, 0, 0.616)",
+//     profileIconColorHover: "black",
+//     searchIconColorHover: "black",
+//     cartIconColorHover: "black",
+//     cartIconMargin: "1vmax",
+// };
+// const Header = () => {
+//     return (
+//         <ReactNavbar  {...options} />
+//     );
+// }
+// export default Header;
+
+
+//---------------------------------------------------------------
+
+import React, { useState } from "react";
+import { AppBar, Toolbar, Tabs, Tab, Typography } from "@material-ui/core";
+import { Link, useHistory } from "react-router-dom";
 import logo from "../../../image/logo.png";
+import { useSelector } from "react-redux";
+import { AccountCircle } from "@material-ui/icons";
 
-const options = {
-
-    logoWidth: "20vmax",
-    logo,
-    burgerColor: "rgba(0, 0, 0, 0.616)",
-    burgerColorHover: " black",
-    navColor1: "white",
-    logoHoverSize: "10px",
-    logoHoverColor: " rgba(0, 0, 0, 0.616)",
-    link1Text: "Trang chủ",
-    link2Text: "Sản phẩm",
-    link3Text: "Liên hệ",
-    link4Text: "Chi tiết",
-    link1Url: "/",
-    link2Url: "/products",
-    link3Url: "/contact",
-    link4Url: "/about",
-    link1Size: "1.3vmax",
-    link1Color: "rgba(0, 0, 0, 0.616)",
-    nav1justifyContent: "flex-end",
-    nav2justifyContent: "flex-end",
-    nav3justifyContent: "flex-start",
-    nav4justifyContent: "flex-start",
-    link1ColorHover: "black",
-    link1Margin: "1vmax",
-    profileIconUrl: "/login",
-    profileIconColor: "rgba(0, 0, 0, 0.616)",
-    searchIconColor: "rgba(0, 0, 0, 0.616)",
-    cartIconColor: "rgba(0, 0, 0, 0.616)",
-    profileIconColorHover: "black",
-    searchIconColorHover: "black",
-    cartIconColorHover: "black",
-    cartIconMargin: "1vmax",
-};
 const Header = () => {
-    return (
-        <ReactNavbar  {...options} />
-    );
-}
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+  const [value, setValue] = useState(0);
+  const history = useHistory();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleLogoClick = () => {
+    setValue(0); // Đặt giá trị value để hiển thị tab trang chủ là đang được chọn
+    history.push("/"); // Điều hướng đến trang chủ
+  };
+
+  return (
+    <AppBar position="static" style={{ backgroundColor: "#ffffff" }}>
+      <Toolbar style={{ justifyContent: "space-between" }}>
+        <Typography variant="h6" component="div" style={{ display: "flex", alignItems: "center", cursor: "pointer" }} onClick={handleLogoClick}>
+          <img src={logo} alt="Logo" style={{ height: "40px", marginRight: "10px" }} />
+          <span style={{ fontWeight: "bold" }}>My Website</span>
+        </Typography>
+        <Tabs value={value} onChange={handleChange} aria-label="navigation" style={{ marginRight: "100px" }}>
+          <Tab label="Trang chủ" component={Link} to="/" style={{ color: "#000000" }} />
+          <Tab label="Sản phẩm" component={Link} to="/products" style={{ color: "#000000" }} />
+          <Tab label="Liên hệ" component={Link} to="/contact" style={{ color: "#000000" }} />
+          <Tab label="Chi tiết" component={Link} to="/about" style={{ color: "#000000" }} />
+          {!isAuthenticated && (
+            <Tab label="Đăng nhập" component={Link} to="/login" style={{ color: "#000000" }} />
+          )}
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
 export default Header;
+
+
+
+
+
+
+
